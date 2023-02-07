ed0e4d61272c95a5e43ee93980700d7c787dc505
kennethjulianda1207@gmail.com <224223.KJulianda@gbs.local>
2023-02-07 12:02:03 +0800

    update

--- www/helloworld.js
+++ www/helloworld.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
 exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'helloworld', 'coolMethod', [arg0]);
+    cordova.exec(success, error, 'helloworld', 'coolMethod', [arg0]);
 };
 
