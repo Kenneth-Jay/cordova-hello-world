4159762e13f9da598d7c4e029e060a57272af854
kennethjulianda1207@gmail.com <224223.KJulianda@gbs.local>
2023-02-07 12:55:22 +0800

    update

--- src/android/helloworld.java
+++ src/android/helloworld.java
@@ -18,7 +18,7 @@ public class helloworld extends CordovaPlugin {
 
             String helloworld = args.getString(0); 
 
-            callbackContext.success(helloworld);
+            callbackContext.success('test');
             
             return true;
         }
